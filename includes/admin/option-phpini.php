a3d5ba7164689a222ee59c0ce4fa0b8ee2bd6e56
Alexander Murashkin <amurashkin@aimk.com>
2024-06-13 17:53:48 -0500

    Fixes

--- includes/admin/option-phpini.php
+++ includes/admin/option-phpini.php
@@ -18,18 +18,16 @@ echo '<div class="full_width_container">
             $path_map_url_prefix = get_option( DDTT_GO_PF.'path_map_url_prefix' );
         }
         
-        trigger_error("$path_map_enable ".$path_map_enable.
-            "$path_map_server_prefix ".$path_map_server_prefix.
-            "$path_map_url_prefix ".$path_map_url_prefix,E_USER_NOTICE ); 
+        trigger_error("path_map_enable ".$path_map_enable.
+            "path_map_server_prefix ".$path_map_server_prefix.
+            "path_map_url_prefix ".$path_map_url_prefix,E_USER_NOTICE ); 
         
         // Cycle through the options
         foreach( $all_options as $option => $value ) {
             
-            if ( $path_map_enable ) {
-                if ( str_starts_with($value,$path_map_server_prefix) ) {
-                    $value = '<a href="'.$path_map_url_prefix.substr($value, strlen($path_map_server_prefix)).
-                             '">'.esc_html( $value ).'</a>';
-                }
+            if ( $path_map_enable && is_string($value) && str_starts_with($value,$path_map_server_prefix) ) {
+                $value = '<a href="'.$path_map_url_prefix.substr($value, strlen($path_map_server_prefix)).
+                         '">'.esc_html( $value ).'</a>';
             } else {
                 $value = esc_html( $value );
             }
